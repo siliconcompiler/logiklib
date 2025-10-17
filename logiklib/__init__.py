c202703ba8acd3cbe1fc33c869c75b3b019f4dbc
petergrossmann21 <peter@zeroasic.com>
2025-10-17 17:08:51 -0400

    Remove superfluous argument

--- logiklib/__init__.py
+++ logiklib/__init__.py
@@ -4,5 +4,4 @@ __version__ = "0.1.0"
 def register_part_data(fpga, package_name, part_name):
     fpga.set_dataroot(
         package_name,
-        f"github://siliconcompiler/logiklib/v{__version__}/{part_name}_cad.tar.gz",
-        f"v{__version__}")
+        f"github://siliconcompiler/logiklib/v{__version__}/{part_name}_cad.tar.gz")
