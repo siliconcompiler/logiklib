480c0d4cc6c866a602b5d55cbc7450c9e317263a
petergrossmann21 <peter@zeroasic.com>
2025-10-20 11:09:15 -0400

    Switch from git API to direct download

--- logiklib/__init__.py
+++ logiklib/__init__.py
@@ -4,4 +4,6 @@ __version__ = "0.1.2"
 def register_part_data(fpga, package_name, part_name):
     fpga.set_dataroot(
         package_name,
-        f"github://siliconcompiler/logiklib/v{__version__}/{part_name}_cad.tar.gz", f"v{__version__}")
+        f"https://github.com/siliconcompiler/logiklib/releases/download/v{__version__}/{part_name}_cad.tar.gz",
+        f'v{__version__}'
+    )
