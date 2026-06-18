b23ecfcb82dc4342a240cf51d254321305b47eab
Peter Grossmann <peter@zeroasic.com>
2026-06-18 15:42:10 -0400

    Fix lint

--- logiklib/zeroasic/z1015/z1015.py
+++ logiklib/zeroasic/z1015/z1015.py
@@ -6,8 +6,6 @@ from logik.devices.logik_fpga import LogikFPGA
 
 from logiklib import register_part_data
 
-import os.path
-
 
 ####################################################
 # Setup for z1015 FPGA
@@ -47,19 +45,160 @@ class z1015(LogikFPGA):
             self.set_yosys_flipfloptechmap('z1015/cad/tech_flops.v')
             self.set_yosys_memorymap(techmap='z1015/cad/tech_bram.v')
             self.set_yosys_memorymap(libmap='z1015/cad/bram_memory_map.txt')
-            self.set_yosys_dsptechmap('z1015/cad/tech_dsp.v', options=['DSP_SIGNEDONLY=1', 'DSP_A_MAXWIDTH=18', 'DSP_B_MAXWIDTH=18', 'DSP_A_MINWIDTH=2', 'DSP_B_MINWIDTH=2', 'DSP_Y_MINWIDTH=2', 'DSP_NAME=_dsp_block_'])
+            self.set_yosys_dsptechmap('z1015/cad/tech_dsp.v',
+                                      options=['DSP_SIGNEDONLY=1',
+                                               'DSP_A_MAXWIDTH=18',
+                                               'DSP_B_MAXWIDTH=18',
+                                               'DSP_A_MINWIDTH=2',
+                                               'DSP_B_MINWIDTH=2',
+                                               'DSP_Y_MINWIDTH=2',
+                                               'DSP_NAME=_dsp_block_'])
             self.add_yosys_macrolib('z1015/cad/tech_dsp_blackbox.v')
 
         # Define the macros that can be techmapped to based on the modes
         # that exist in the design
-        self.add_yosys_registertype(['dff', 'dffe', 'dffeh', 'dffehl', 'dffehlr', 'dffehlrs', 'dffehls', 'dffehr', 'dffehrs', 'dffehs', 'dffel', 'dffelr', 'dffelrs', 'dffels', 'dffer', 'dffers', 'dffes', 'dffh', 'dffhl', 'dffhlr', 'dffhlrs', 'dffhls', 'dffhr', 'dffhrs', 'dffhs', 'dffl', 'dfflr', 'dfflrs', 'dffls', 'dffr', 'dffrs', 'dffs'])
-        self.add_vpr_registertype(['dff', 'dffe', 'dffeh', 'dffehl', 'dffehlr', 'dffehlrs', 'dffehls', 'dffehr', 'dffehrs', 'dffehs', 'dffel', 'dffelr', 'dffelrs', 'dffels', 'dffer', 'dffers', 'dffes', 'dffh', 'dffhl', 'dffhlr', 'dffhlrs', 'dffhls', 'dffhr', 'dffhrs', 'dffhs', 'dffl', 'dfflr', 'dfflrs', 'dffls', 'dffr', 'dffrs', 'dffs'])
+        self.add_yosys_registertype(['dff',
+                                     'dffe',
+                                     'dffeh',
+                                     'dffehl',
+                                     'dffehlr',
+                                     'dffehlrs',
+                                     'dffehls',
+                                     'dffehr',
+                                     'dffehrs',
+                                     'dffehs',
+                                     'dffel',
+                                     'dffelr',
+                                     'dffelrs',
+                                     'dffels',
+                                     'dffer',
+                                     'dffers',
+                                     'dffes',
+                                     'dffh',
+                                     'dffhl',
+                                     'dffhlr',
+                                     'dffhlrs',
+                                     'dffhls',
+                                     'dffhr',
+                                     'dffhrs',
+                                     'dffhs',
+                                     'dffl',
+                                     'dfflr',
+                                     'dfflrs',
+                                     'dffls',
+                                     'dffr',
+                                     'dffrs',
+                                     'dffs'])
+        self.add_vpr_registertype(['dff',
+                                   'dffe',
+                                   'dffeh',
+                                   'dffehl',
+                                   'dffehlr',
+                                   'dffehlrs',
+                                   'dffehls',
+                                   'dffehr',
+                                   'dffehrs',
+                                   'dffehs',
+                                   'dffel',
+                                   'dffelr',
+                                   'dffelrs',
+                                   'dffels',
+                                   'dffer',
+                                   'dffers',
+                                   'dffes',
+                                   'dffh',
+                                   'dffhl',
+                                   'dffhlr',
+                                   'dffhlrs',
+                                   'dffhls',
+                                   'dffhr',
+                                   'dffhrs',
+                                   'dffhs',
+                                   'dffl',
+                                   'dfflr',
+                                   'dfflrs',
+                                   'dffls',
+                                   'dffr',
+                                   'dffrs',
+                                   'dffs'])
 
-        self.add_yosys_dsptype(['dsp_mult', 'efpga_acc', 'efpga_acc_regi', 'efpga_adder', 'efpga_adder_regi', 'efpga_adder_regio', 'efpga_adder_rego', 'efpga_macc', 'efpga_macc_pipe', 'efpga_macc_pipe_regi', 'efpga_macc_regi', 'efpga_mult', 'efpga_mult_addc', 'efpga_mult_addc_regi', 'efpga_mult_addc_regio', 'efpga_mult_addc_rego', 'efpga_mult_regi', 'efpga_mult_regio', 'efpga_mult_rego'])
-        self.add_vpr_dsptype(['dsp_mult', 'efpga_acc', 'efpga_acc_regi', 'efpga_adder', 'efpga_adder_regi', 'efpga_adder_regio', 'efpga_adder_rego', 'efpga_macc', 'efpga_macc_pipe', 'efpga_macc_pipe_regi', 'efpga_macc_regi', 'efpga_mult', 'efpga_mult_addc', 'efpga_mult_addc_regi', 'efpga_mult_addc_regio', 'efpga_mult_addc_rego', 'efpga_mult_regi', 'efpga_mult_regio', 'efpga_mult_rego'])
+        self.add_yosys_dsptype(['dsp_mult',
+                                'efpga_acc',
+                                'efpga_acc_regi',
+                                'efpga_adder',
+                                'efpga_adder_regi',
+                                'efpga_adder_regio',
+                                'efpga_adder_rego',
+                                'efpga_macc',
+                                'efpga_macc_pipe',
+                                'efpga_macc_pipe_regi',
+                                'efpga_macc_regi',
+                                'efpga_mult',
+                                'efpga_mult_addc',
+                                'efpga_mult_addc_regi',
+                                'efpga_mult_addc_regio',
+                                'efpga_mult_addc_rego',
+                                'efpga_mult_regi',
+                                'efpga_mult_regio',
+                                'efpga_mult_rego'])
+        self.add_vpr_dsptype(['dsp_mult',
+                              'efpga_acc',
+                              'efpga_acc_regi',
+                              'efpga_adder',
+                              'efpga_adder_regi',
+                              'efpga_adder_regio',
+                              'efpga_adder_rego',
+                              'efpga_macc',
+                              'efpga_macc_pipe',
+                              'efpga_macc_pipe_regi',
+                              'efpga_macc_regi',
+                              'efpga_mult',
+                              'efpga_mult_addc',
+                              'efpga_mult_addc_regi',
+                              'efpga_mult_addc_regio',
+                              'efpga_mult_addc_rego',
+                              'efpga_mult_regi',
+                              'efpga_mult_regio',
+                              'efpga_mult_rego'])
 
-        self.add_yosys_bramtype(['sdpram_1024x16', 'sdpram_16384x1', 'sdpram_2048x8', 'sdpram_4096x4', 'sdpram_8192x2', 'spram_1024x16', 'spram_16384x1', 'spram_2048x8', 'spram_4096x4', 'spram_512x32', 'spram_8192x2', 'sram_sdp', 'sram_sp', 'sram_tdp', 'tdpram_1024x16', 'tdpram_16384x1', 'tdpram_2048x8', 'tdpram_4096x4', 'tdpram_8192x2'])
-        self.add_vpr_bramtype(['sdpram_1024x16', 'sdpram_16384x1', 'sdpram_2048x8', 'sdpram_4096x4', 'sdpram_8192x2', 'spram_1024x16', 'spram_16384x1', 'spram_2048x8', 'spram_4096x4', 'spram_512x32', 'spram_8192x2', 'sram_sdp', 'sram_sp', 'sram_tdp', 'tdpram_1024x16', 'tdpram_16384x1', 'tdpram_2048x8', 'tdpram_4096x4', 'tdpram_8192x2'])
+        self.add_yosys_bramtype(['sdpram_1024x16',
+                                 'sdpram_16384x1',
+                                 'sdpram_2048x8',
+                                 'sdpram_4096x4',
+                                 'sdpram_8192x2',
+                                 'spram_1024x16',
+                                 'spram_16384x1',
+                                 'spram_2048x8',
+                                 'spram_4096x4',
+                                 'spram_512x32',
+                                 'spram_8192x2',
+                                 'sram_sdp',
+                                 'sram_sp',
+                                 'sram_tdp',
+                                 'tdpram_1024x16',
+                                 'tdpram_16384x1',
+                                 'tdpram_2048x8',
+                                 'tdpram_4096x4',
+                                 'tdpram_8192x2'])
+        self.add_vpr_bramtype(['sdpram_1024x16',
+                               'sdpram_16384x1',
+                               'sdpram_2048x8',
+                               'sdpram_4096x4',
+                               'sdpram_8192x2',
+                               'spram_1024x16',
+                               'spram_16384x1',
+                               'spram_2048x8',
+                               'spram_4096x4',
+                               'spram_512x32',
+                               'spram_8192x2',
+                               'sram_sdp',
+                               'sram_sp',
+                               'sram_tdp',
+                               'tdpram_1024x16',
+                               'tdpram_16384x1',
+                               'tdpram_2048x8',
+                               'tdpram_4096x4',
+                               'tdpram_8192x2'])
 
         # TODO: blackbox_options
 
